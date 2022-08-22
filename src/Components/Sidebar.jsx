e467f8de300fe0320f01c1df7cf0a7eaa717caf1
Anowar Hossain <anowar3042@gmail.com>
2022-08-22 18:44:27 +0530

    done

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,16 +1,45 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Sidebar = () => {
+  const [searchParams, setSearchParams] = useSearchParams({});
+  const initialSoryByParams = searchParams.get("sortBy");
+
+  const [sortBy, setSortBy] = useState(initialSoryByParams || "");
+  const handleSortBy = (e) => {
+    setSortBy(e.target.value);
+  };
+  console.log(sortBy);
+  useEffect(() => {
+    if (sortBy) {
+      const params = {};
+      sortBy && (params.sortBy = sortBy);
+      //setSearchParams({ genre: category, sortBy });
+      setSearchParams(params);
+    }
+  }, [sortBy, searchParams]);
+
   return (
     <div style={{ minWidth: "250px" }}>
       <h3>Sort By</h3>
-      <div>
+      <div onChange={handleSortBy}>
         <div>
-          <input type="radio" data-cy="asc" value="asc" />
+          <input
+            type="radio"
+            data-cy="asc"
+            value="asc"
+            defaultChecked={sortBy === "asc"}
+          />
           <label>Ascending</label>
         </div>
         <div>
-          <input type="radio" data-cy="desc" value="desc" />
+          <input
+            type="radio"
+            data-cy="desc"
+            value="desc"
+            defaultChecked={sortBy === "desc"}
+          />
           <label>Descending</label>
         </div>
       </div>
